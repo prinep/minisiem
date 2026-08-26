05bda337dc7fb5ef5c0bd0741f370e5ef17cdd32
prinep <prishanepal2020+1@gmail.com>
2026-08-27 00:28:33 +0545

    Add SQLite event and alert storage

---
+++ src/database.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db_name="minisiem.db"):
+        self.connection = sqlite3.connect(db_name)
+        self.create_tables()
+
+    def create_tables(self):
+        cursor = self.connection.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS events (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TEXT,
+                event TEXT,
+                username TEXT,
+                ip TEXT
+            )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS alerts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                message TEXT,
+                severity TEXT,
+                ip TEXT,
+                rule TEXT
+            )
+        """)
+
+    def save_event(self, event):
+        cursor = self.connection.cursor()
+
+        cursor.execute("""
+            INSERT INTO events (timestamp, event, username, ip)
+            VALUES (?, ?, ?, ?)
+        """, (
+            event.timestamp,
+            event.event,
+            event.username,
+            event.ip
+        ))
+
+        self.connection.commit()
+
+    def save_alert(self, alert):
+        cursor = self.connection.cursor()
+
+        cursor.execute("""
+            INSERT INTO alerts (message, severity, ip, rule)
+            VALUES (?, ?, ?, ?)
+        """, (
+            alert.message,
+            alert.severity,
+            alert.ip,
+            alert.rule
+        ))
+
+        self.connection.commit()
+
+        
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from parser import parse_log
 from detector import DetectionEngine
+from database import Database
 
 
 detector = DetectionEngine()
+database = Database()
 
 with open("logs/sample.log", "r") as file:
     logs = file.readlines()
@@ -12,9 +14,14 @@ for log in logs:
     event = parse_log(log)
 
     if event:
+        database.save_event(event)
+
         alerts = detector.analyze(event)
 
         for alert in alerts:
+
+            database.save_alert(alert)
+            
             print("ALERT:", alert.message)
             print("Severity:", alert.severity)
             print("IP:", alert.ip)
