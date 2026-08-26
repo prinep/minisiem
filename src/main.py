2251e736e3cfbed70f303bc556cc0e44dfd800f1
prinep <prishanepal2020+1@gmail.com>
2026-08-26 23:27:21 +0545

    Build initial log parser

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("MiniSIEM starting...")
+import re
+
+with open("logs/sample.log", "r") as file:
+    logs = file.readlines()
+
+for log in logs:
+    match = re.search(
+        r"(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) sshd: (Failed|Accepted) password for (\w+) from (\d+\.\d+\.\d+\.\d+)",
+        log
+    )
+
+    if match:
+        timestamp = match.group(1)
+        event = match.group(2)
+        username = match.group(3)
+        ip = match.group(4)
+
+        print("Timestamp:", timestamp)
+        print("Event:", event)
+        print("Username:", username)
+        print("IP:", ip)
+        print()
